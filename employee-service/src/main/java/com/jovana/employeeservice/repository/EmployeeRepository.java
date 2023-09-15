56ed9f8c766c279c02568ba8ebe2f88086d11a49
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-09-15 10:51:14 +0200

    Added global exception handling for employee service

--- employee-service/src/main/java/com/jovana/employeeservice/repository/EmployeeRepository.java
+++ employee-service/src/main/java/com/jovana/employeeservice/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.jovana.employeeservice.repository;
 import com.jovana.employeeservice.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import java.util.Optional;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    
+    Optional<Employee> findByEmail (String email);
 }
--- employee-service/src/main/java/com/jovana/employeeservice/service/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/jovana/employeeservice/service/EmployeeServiceImpl.java
@@ -2,10 +2,14 @@ package com.jovana.employeeservice.service;
 
 import com.jovana.employeeservice.dto.EmployeeDto;
 import com.jovana.employeeservice.entity.Employee;
+import com.jovana.employeeservice.exception.EmployeeAlreadyExistsException;
+import com.jovana.employeeservice.exception.ResourceNotFoundException;
 import com.jovana.employeeservice.mapper.EmployeeMapper;
 import com.jovana.employeeservice.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,14 +18,21 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeMapper employeeMapper;
     @Override
     public EmployeeDto saveEmployee (EmployeeDto employeeDto) {
-       // Employee employee = employeeMapper.employeeDtoToEmployee(employeeDto)
-        Employee employee = employeeRepository.save(employeeMapper.employeeDtoToEmployee(employeeDto));
-        return employeeMapper.employeeToEmployeeDto(employee);
+        Optional<Employee> employee =
+                employeeRepository.findByEmail(employeeDto.email());
+        if (employee.isPresent()) {
+            throw new EmployeeAlreadyExistsException("Employee already exists.");
+        }
+        return employeeMapper.employeeToEmployeeDto(employeeRepository.save(employeeMapper.employeeDtoToEmployee(
+                employeeDto)));
     }
     
     @Override
     public EmployeeDto getEmployeeById (Long id) {
-        Employee employee = employeeRepository.findById(id).orElseThrow();
+        Employee employee = employeeRepository.findById(id)
+                                              .orElseThrow(() -> new ResourceNotFoundException("Employee",
+                                                                                               "id",
+                                                                                               id.toString()));
         return employeeMapper.employeeToEmployeeDto(employee);
     }
 }
