0c8da39374ea74757a3165ddc6cf1647f6dcb72f
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-09-15 10:54:23 +0200

    Added global exception handling for department service

--- department-service/src/main/java/com/jovana/departmentservice/service/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/jovana/departmentservice/service/DepartmentServiceImpl.java
@@ -2,6 +2,7 @@ package com.jovana.departmentservice.service;
 
 import com.jovana.departmentservice.dto.DepartmentDto;
 import com.jovana.departmentservice.entity.Department;
+import com.jovana.departmentservice.exception.ResourceNotFoundException;
 import com.jovana.departmentservice.mapper.DepartmentMapper;
 import com.jovana.departmentservice.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,10 @@ public class DepartmentServiceImpl implements DepartmentService {
     
     @Override
     public DepartmentDto getDepartmentByCode (String code) {
-        return departmentMapper.departmentToDepartmentDto(departmentRepository.findByCode(code).orElseThrow());
+        Department department =
+                departmentRepository.findByCode(code)
+                                    .orElseThrow(() -> new ResourceNotFoundException("Department",
+                                                                                     "code", code));
+        return departmentMapper.departmentToDepartmentDto(department);
     }
 }
